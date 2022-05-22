fcbbb74055ebe17d2373e637b18d03b8e372d99c
xanguyen <xavier.nguyen@student.uclouvain.be>
2022-05-22 18:04:40 +0200

    rules update

--- rules.py
+++ rules.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def dot_num_in_domain(domain):
 	count = 0
 	for c in domain:
@@ -32,3 +34,16 @@ def number_of_special_char_in_domain(domain):
 
 def num_request(requests):
 	return len(requests)
+
+def first_last_window(the_host, the_list):
+	#np.column_stack((thelist[0], thelist[1]))
+	first = -1
+	last = -1
+	for timestamp, host in the_list:
+		if host==the_host:
+			if first == -1:
+				first = timestamp
+			
+			last = timestamp
+
+	return [first, last]
